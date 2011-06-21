e63265bc3b41577191f4a1deb5100145601a0c33
Kousuke Ebihara <ebihara@php.net>
2011-06-21 18:59:27 +0900

    added tests for opExecutionFilter::needToRetriveMobileUID() (refs #2225)

--- test/unit/filter/opExecutionFilterTest.php
+++ test/unit/filter/opExecutionFilterTest.php
@@ -93,12 +93,23 @@ class myFilter extends opExecutionFilter
   {
     $this->spec_change_date = $date_str;
   }
+
+  public function callNeedToRetriveMobileUID($module, $action, $retriveUIDMode = 1, $parameters = array())
+  {
+    $sslSelectableList = sfConfig::get('op_ssl_selectable_actions', array(
+        sfConfig::get('sf_app') => array(),
+    ));
+
+    opConfig::set('retrive_uid', $retriveUIDMode);
+
+    return $this->needToRetriveMobileUID($module, $action, new opWebRequest(new sfEventDispatcher(), $parameters), $sslSelectableList);
+  }
 }
 
-$t = new lime_test(2);
+$t = new lime_test(7);
 
 require_once dirname(__FILE__).'/../../../config/ProjectConfiguration.class.php';
-$configuration = ProjectConfiguration::getApplicationConfiguration('pc_frontend', 'test', true);
+$configuration = ProjectConfiguration::getApplicationConfiguration('mobile_frontend', 'test', true);
 testContext::createInstance($configuration);
 $context = testContext::getInstance();
 
@@ -114,3 +125,10 @@ $t->diag('spec_change_date > test_date');
 $filter->setTestDate(date('Y-m-d H:i:s', time() + 60));
 $actionInstance = new testAction($context, $context->getModuleName(), $context->getActionName());
 $t->is($filter->handleAction($filterChain, $actionInstance), "redirect", "before spec_change_date, redirect to SoftBank GW");
+
+$t->diag('Test for redirecting to HTTP action for retriving mobile UID');
+$t->ok($filter->callNeedToRetriveMobileUID('member', 'configUID'), 'member/configUID redirects user to HTTP');
+$t->ok($filter->callNeedToRetriveMobileUID('member', 'configUID', 0), 'member/configUID does not redirects user to HTTP when it does not retrieve uid');
+$t->ok(!$filter->callNeedToRetriveMobileUID('member', 'home'), 'member/home does not redirect user to HTTP');
+$t->ok(!$filter->callNeedToRetriveMobileUID('member', 'login'), 'member/login does not redirect user to HTTP');
+$t->ok($filter->callNeedToRetriveMobileUID('member', 'login', 1, array('authMode' => 'MobileUID')), 'member/login redirect user to HTTP when the authMode is MobileUID');
